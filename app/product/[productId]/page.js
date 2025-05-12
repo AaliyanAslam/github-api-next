bda82754d6546ba8b1b2d3b22721f5861ae326b9
Adil Aslam <aaliyanaslam22@gmail.com>
2025-05-12 17:32:52 +0500

    daisyi ui i , tailwind i ,

--- app/product/[productId]/page.js
+++ app/product/[productId]/page.js
@@ -1,16 +1,17 @@
-import axios from "axios";
+// import axios from "axios";
+import Image from "next/image";
 import React from "react";
 
 const page = async ({ params }) => {
   const { productId } = await params;
-
+let data = null
   try {
     const api = "https://api.github.com/users";
 
-    const res = await axios(`${api}/${productId}`);
-    // const data = await res.json();
+    const res = await fetch(`${api}/${productId}`);
+   data = await res.json();
 
-    console.log(res.data);
+    console.log(data);
   } catch (error) {
     console.log(error);
   }
@@ -18,10 +19,35 @@ const page = async ({ params }) => {
     <>
       {/* {console.log(productId)} */}
 
-      <div>
+      {/* <div>
         <h1 style={{ fontFamily: "sans", textAlign: "center" }}>{productId}</h1>
-        {/* <p>{console.log(res.data)}</p> */}
-      </div>
+        <Image
+          src={data?.avatar_url}
+          alt="image"
+          width={200}
+          height={200}
+          style={{ borderRadius: "50%", margin: "auto", display: "block" }}/>
+        <p className="text-center">{data.name}</p>
+      </div> */}
+
+<div className="card card-side bg-base-100 shadow-sm">
+  <figure>
+  <Image
+          src={data?.avatar_url}
+          alt="image"
+          width={200}
+          height={200}
+          priority
+         />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">New movie is released!</h2>
+    <p>Click the button to watch on Jetflix app.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Watch</button>
+    </div>
+  </div>
+</div>
     </>
   );
 };
