3c2d008825368b5c264d5613f0504ea5ceb5ae0d
Adil Aslam <aaliyanaslam22@gmail.com>
2025-05-13 11:51:40 +0500

    input compo done , user data shown , navigate to dynamic page  user detail is working

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
+import Input from "@/components/Input";
 
 
 export default function Home() {
   return (
- <>this is home</>
+ <>this is home
+ <Input/>
+ 
+ </>
   );
 }
--- components/Input.js
+++ components/Input.js
@@ -1,10 +1,36 @@
-import React from 'react'
+"use client";
+import { useForm } from "react-hook-form";
+import React  from "react";
+import { useRouter } from "next/navigation";
 
-const Input = () => {
+export default function Input() {
+const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) =>{
+    console.watch(data)
+    console.log(data)
+    router.push(`/product/${data.username}`)
+  } 
+
+
+  
   return (
-    <>
-    </>
-  )
-}
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+   
 
-export default Input
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("username", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
+  );
+}
