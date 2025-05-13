d5b045d3854d384ca6e427613832843fb44c6507
Adil Aslam <aaliyanaslam22@gmail.com>
2025-05-13 12:40:36 +0500

    react icon i  , btn func added

--- components/BackBtn.js
+++ components/BackBtn.js
@@ -1,12 +1,20 @@
 "use client";
-import React from 'react'
-import { useRouter } from 'next/navigation'
+import React from "react";
+import { useRouter } from "next/navigation";
+import { MdOutlineArrowBackIosNew } from "react-icons/md";
 
 const BackBtn = () => {
-    const router = useRouter();
+  const router = useRouter();
   return (
-    <div><button className="btn btn-soft btn-warning" onClick={()=> router.push("/") }>Warning</button></div>
-  )
-}
+    <div className="relative bottom-5">
+      <button
+        className="btn btn-dark hover:bg-[#ff3a3a] border-none bg-white hover:text-white"
+        onClick={() => router.push("/")}
+      >
+        <MdOutlineArrowBackIosNew />
+      </button>
+    </div>
+  );
+};
 
-export default BackBtn
+export default BackBtn;
